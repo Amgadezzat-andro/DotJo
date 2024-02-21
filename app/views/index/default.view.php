1b51984f5b2bbf495a30d2d0b21ae143ca819c49
Amgad Ezzat <megostar59@gmail.com>
2024-02-21 05:15:47 +0200

    Just Adding Some Index Info

--- app/views/index/default.view.php
+++ app/views/index/default.view.php
@@ -2,3 +2,9 @@
 <h2>It Could Be A Dashboard but it's so lonely in here 😊 </h2>
 <h2>Database Name : dotjo_task</h2>
 <h2>You Can Find DATABASE CREDENTIALS in /app/config/config.php</h2>
+<br>
+<br>
+<h2>INFO</h2>
+<h3 style="color: red;" >you need to create category first then create articles</h2>
+<h3 style="color: red;">each article belong to a specific category (one to many relationship)</h3>
+<h3 style="color: red;">pagination works Dynmaically -> after adding an article</h3>
